2d838612abcb228615be4997f22d98a8540421bd
EliAlas11 <eliasnizar279@gmail.com>
2025-05-29 14:39:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -195,4 +195,8 @@ def device_info():
 
 # تشغيل التطبيق
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
